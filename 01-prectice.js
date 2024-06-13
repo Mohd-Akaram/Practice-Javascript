7859e7871e0e78c002b3f5f5346128a052488ebd
Mohd Akaram <127597055+Mohd-Akaram@users.noreply.github.com>
2024-06-13 09:48:16 +0000

    Javascript File

---
+++ 01-prectice.js
@@ -0,0 +1,16 @@
+const username = "akaram"
+let userPassword = 111222
+var addUser = "mumbai"
+
+contryUser = "India"
+
+
+
+
+
+
+console.table(username,
+    userPassword,
+    addUser,
+
+    contryUser)
